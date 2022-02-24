1043455f8e77a27f0733016544c0b43d3dae3105
Ashok Koda <kodaashok60@gmail.com>
2022-02-24 20:14:56 +0530

    Added:Filp coin program done

---
+++ src/main/java/filpcoin/FlipCoin.java
@@ -0,0 +1,37 @@
+package filpcoin;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class FlipCoin {
+
+	public static void main(String[] args) {
+		DecimalFormat decimalFormat = new DecimalFormat("##.00");
+		System.out.println("How many times you want to flip the coin?");
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+
+		int headCount = 0, tailCount = 0;
+
+		for (int i = 0; i < count; i++) {
+			if (Math.random() < 0.5) {
+				tailCount++;
+			} else {
+				headCount++;
+			}
+		}
+
+		System.out.println("Head count : " + headCount);
+		System.out.println("Tail count : " + tailCount);
+
+		String headPercentage = decimalFormat.format(((double) headCount / count) * 100);
+		String tailPercentage = decimalFormat.format(((double) tailCount / count) * 100);
+
+		System.out.println("Head Percentage : " + headPercentage + "%");
+		System.out.println("Tail Percentage : " + tailPercentage + "%");
+
+		sc.close();
+
+	}
+
+}
