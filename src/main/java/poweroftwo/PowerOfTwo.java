d54a41c05ebf0ca154e9f25cef8b93690c4809bb
Ashok Koda <kodaashok60@gmail.com>
2022-02-25 18:44:25 +0530

    Added:program to calculate the power of 2

---
+++ src/main/java/poweroftwo/PowerOfTwo.java
@@ -0,0 +1,22 @@
+package poweroftwo;
+
+import java.util.Scanner;
+
+public class PowerOfTwo {
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		int number = 2, i, exponent;
+        long power = 1;
+        sc = new Scanner(System.in);
+
+        System.out.print(" Please Enter the Exponent Value : ");
+        exponent = sc.nextInt();
+
+        for (i = 1; i <= exponent; i++) {
+            power = power * number;
+        }
+        System.out.println("\n The Final result of " + number + " power " + exponent + " = " + power);
+	}
+
+}
