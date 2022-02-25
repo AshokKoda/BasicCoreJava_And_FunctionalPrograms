c1603e125fb10de5d821fdfc8d10a96011e9c661
Ashok Koda <kodaashok60@gmail.com>
2022-02-25 19:38:33 +0530

    Added the functionality to compute the largest of three numbers

--- src/main/java/bridgelabz/com/FindLargestNumber.java
+++ src/main/java/bridgelabz/com/FindLargestNumber.java
@@ -5,23 +5,29 @@ import java.util.Scanner;
 public class FindLargestNumber {
 
 	public static void main(String[] args) {
+		
+		int a, b, c;
 
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the first number: ");
-		int x = sc.nextInt();
+		a = sc.nextInt();
 		System.out.print("Enter the second number: ");
-		int y = sc.nextInt();
+		b = sc.nextInt();
 		System.out.print("Enter the third number: ");
-		int z = sc.nextInt();
+		c = sc.nextInt();
 
 		System.out.println("<------------------------------->");
-		if (x > y && x > z) {
-			System.out.println("Largest number is: " + x);
-		} else if (y > z) {
-			System.out.println("Largest number is: " + y);
+		if (a > b && a > c) {
+			System.out.println("Largest number is: " + a);
+		} else if (b > c) {
+			System.out.println("Largest number is: " + b);
 		} else {
-			System.out.println("Largest number is: " + z);
+			System.out.println("Largest number is: " + c);
 		}
+		
+		System.out.println("<-------------Ternary Logic Output------------------>");
+		int largest = c > (a > b ? a : b) ? c : ((a > b) ? a : b);
+		System.out.println("The largest number is: " + largest);
 
 	}
 
