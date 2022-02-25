aed89c75cd9872bb8f6f56e5a47315633972117d
Ashok Koda <kodaashok60@gmail.com>
2022-02-25 19:24:44 +0530

    Added:program to check if the given input is Vowel or consonant

---
+++ src/main/java/bridgelabz/com/VowelAndConsonant.java
@@ -0,0 +1,27 @@
+package bridgelabz.com;
+
+import java.util.Scanner;
+
+public class VowelAndConsonant {
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter a character :");
+		Scanner sc = new Scanner(System.in);
+		char ch = sc.next().charAt(0);
+
+		switch (ch) {
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+			System.out.println(ch + " is vowel");
+			break;
+		default:
+			System.out.println(ch + " is consonant");
+		}
+
+	}
+
+}
